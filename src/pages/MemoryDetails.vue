0699e476f35e60b7e32e17cb2949b4c387aeb823
TouatiChahrazed <fc_touati@esi.dz>
2020-12-30 14:14:25 +0100

    Styling: css generator, basic components: ionPage, ionContent ...

---
+++ src/pages/MemoryDetails.vue
@@ -0,0 +1,40 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>All Memories</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+        <ion-item> Chahrazed </ion-item>
+        <ion-item> Marwa </ion-item>
+        <ion-item> Sihem </ion-item>
+      </ion-list>
+    </ion-content>
+   </ion-page>
+</template>
+
+<script>
+import {
+  IonPage, 
+  IonContent,
+  IonToolbar,
+  IonHeader,
+  IonTitle,
+  IonList,
+  IonItem
+} from '@ionic/vue'
+export default {
+  components: {
+    IonContent,
+    IonToolbar,
+    IonTitle,
+    IonPage,
+    IonHeader,
+    IonList,
+    IonItem
+  }
+
+}
+</script>
